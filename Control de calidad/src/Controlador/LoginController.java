291a0b7d7ee44a81e18382de79f4bcfc25ceb278
Micaela A. Paez <paezmicaelaagustina@gmail.com>
2023-04-25 22:53:11 -0300

    ventana y controladores (solo inicializados) de login, menu y op

---
+++ Control de calidad/src/Controlador/LoginController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+import Vista.VistaLogin;
+import Vista.*;
+
+public class LoginController {
+    public static VistaLogin  ventana = new VistaLogin();
+    public static void mostrar(){ ventana.setVisible(true);}
+    public static void ocultar(){ventana.setVisible(false);}
+    
+    public static void botonContinuar()
+    {
+      String usuario=  ventana.getjTextField1().getText(); 
+       String clave = ventana.getjTextField2().getText();
+       if (usuario.equals("admin") && clave.equalsIgnoreCase("1234") ){
+           MenuController.mostrar();
+       }
+       else{
+           System.out.println("Usuario o Clave incorrectos"); 
+       }
+    }
+    
+}
