2ff3d9e990f88f1ce961bc2918b90153c774b901
Facundo Barrera <barreraaybarfacundo@gmail.com>
2023-04-25 23:19:13 -0300

    Corregir error en vistas

--- Control de calidad/src/control/de/calidad/ControlDeCalidad.java
+++ Control de calidad/src/control/de/calidad/ControlDeCalidad.java
@@ -4,6 +4,9 @@
  */
 package control.de.calidad;
 
+import Vista.VistaLogin;
+import Vista.VistaMenu;
+
 /**
  *
  * @author User
@@ -14,7 +17,7 @@ public class ControlDeCalidad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new VistaLogin().setVisible(true);
     }
     
 }
