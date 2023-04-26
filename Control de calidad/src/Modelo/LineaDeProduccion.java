fd1c8c43ddc7a7097a3562b9da376fcbdcf4cd36
luciano <luc02@live.com>
2023-04-25 21:27:30 -0300

    Con paquetes

--- Control de calidad/src/Modelo/LineaDeProduccion.java
+++ Control de calidad/src/Modelo/LineaDeProduccion.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package control.de.calidad;
+package Modelo;
 
 /**
  *
  * @author User
  */
-public class Vista {
+public class LineaDeProduccion {
     
 }
